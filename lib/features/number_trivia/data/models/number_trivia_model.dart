48b0af673553014ab145a8751afa4d55d6a6f97c
admin <admin@admins-MacBook-Pro.local>
2021-05-13 21:04:20 +0530

    Domain Layer Implemented and Implementing Data Layer done with repository impl file

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,14 @@
+import 'package:clean_architecture_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:meta/meta.dart';
+
+class NumberTriviaModel extends NumberTrivia{
+  NumberTriviaModel({
+    @required String text,
+    @required int number
+}):super(text: text,number: number);
+
+  factory NumberTriviaModel.fromJson(Map<String,dynamic> json){
+    return NumberTriviaModel(text:json['text'],
+        number:(json['number'] as num).toInt());
+  }
+}
---
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,64 @@
+import 'package:clean_architecture_app/core/errors/exceptions.dart';
+import 'package:clean_architecture_app/core/errors/failures.dart';
+import 'package:clean_architecture_app/core/platform/network_info.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'package:clean_architecture_app/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:clean_architecture_app/features/number_trivia/domain/repositories/number_trivia_repositories.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository{
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl({@required this.remoteDataSource,@required this.localDataSource,@required this.networkInfo});
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) async{
+    // TODO: implement getConcreteNumberTrivia
+
+    if(await networkInfo.isConnected){
+      try{
+        final remoteTrivia=await remoteDataSource.getConcreteNumberTrivia(number);
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      }
+      on ServerException{
+        return Left(ServerFailure());
+      }
+    } else{
+      try{
+        final localTrivia= await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      }
+      on CacheException{
+        return Left(CacheFailure());
+      }
+    }
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() async{
+    // TODO: implement getRandomNumberTrivia
+    if(await networkInfo.isConnected){
+      try{
+        final remoteTrivia=await remoteDataSource.getRandomNumberTrivia();
+        localDataSource.cacheNumberTrivia(remoteTrivia);
+        return Right(remoteTrivia);
+      }
+      on ServerException{
+        return Left(ServerFailure());
+      }
+    } else{
+      try{
+        final localTrivia= await localDataSource.getLastNumberTrivia();
+        return Right(localTrivia);
+      }
+      on CacheException{
+        return Left(CacheFailure());
+      }
+    }
+  }
+}
